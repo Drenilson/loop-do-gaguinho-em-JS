9f4a3a02eaf2376be32f968bb6c62c6452386a94
Andre_Luiz <andre_luiz1907@hotmail.com>
2024-09-11 20:44:18 -0300

    primeiro arquivo

---
+++ index.js
@@ -0,0 +1,7 @@
+// loop do gaguinho é uma brincadeira baseada no personagem Gaguinho do desenho Looney Tunes.
+
+let fala = ["É isso ai", "Pe...", "Pe...", "Pe...", "Pessoal!"]
+
+for(let i = 0; i < fala.length; i++) {
+    console.log(fala[i]);
+};
